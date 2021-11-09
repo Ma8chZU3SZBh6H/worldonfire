eb46c212d3e2e6968cffd2f2c0999318d2f2590e
skindervik <38836685+skindervik@users.noreply.github.com>
2021-11-09 14:40:34 +0200

    browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,6 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.ts("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [require("tailwindcss")]
-);
+mix.ts("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .browserSync("http://localhost/");
